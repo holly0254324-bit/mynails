b32b46b3caf3d67bd6d49bdaa1a0889716e1005f
holly0254324-bit <0254324@gmail.com>
2026-05-05 16:45:07 +0300

    Add configuration for bot with environment variables

---
+++ bot/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+WORK_START = 9
+WORK_END = 20
+
+SLOT_STEP_MINUTES = 30
+
+SERVICE_DURATIONS = {
+    "manicure": 90,
+    "pedicure": 90,
+    "combo": 180,
+}
